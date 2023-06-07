a5ba21c26ab196b2d55ae9aea9e067c5319ebd27
natalia.puig <natalia.puig@basetis.com>
2023-06-07 13:31:21 +0200

    filters

---
+++ pages/filter.vue
@@ -0,0 +1,82 @@
+<script>
+export default {
+  async asyncData({ params, redirect, error, $axios, $claims }) {
+    const data = await fetch("https://bibliotech.bymotto.com/api/searcher")
+      .then((res) => res.json())
+      .then((data) => data.data);
+    return { data };
+  },
+  data() {
+    return { nameName: "", description: "" };
+  },
+
+  methods: {
+    async applyFilters() {
+      console.log("apply filters");
+      this.data = await fetch(
+        `https://bibliotech.bymotto.com/api/searcher?name=${this.nameName}&description=${this.description}`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            name: this.nameName,
+            description: this.description,
+          }),
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => data.data);
+      console.log("fata", this.data);
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Head>
+      <Title>We bcn | Filter</Title>
+      <Meta
+        name="Description"
+        content="We bcn is a company especialized in technology..."
+      ></Meta>
+    </Head>
+
+    <nav>
+      <ul>
+        <li><RouterLink :to="'/'">Home</RouterLink></li>
+      </ul>
+    </nav>
+    <h2>Busqueda:</h2>
+    <br />
+    <form @submit.prevent>
+      <input v-model="nameName" type="text" name="name" placeholder="nombre" />
+      <input
+        v-model="description"
+        type="text"
+        name="description"
+        placeholder="descripción"
+      />
+      <button @click="applyFilters()">Aplicar filtros</button>
+    </form>
+    <br /><br />
+    <div>
+      <div class="product" v-for="(product, i) in data" :key="i">
+        <RouterLink :to="`/details/${product.id}`">
+          <div>
+            {{ product.name }}
+          </div>
+        </RouterLink>
+        <div v-html="product.description"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+  width: 30%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,83 @@
+<script>
+// import { useConfigStore } from '~/store/config';
+
+// const configStore = useConfigStore()
+export default {
+  async asyncData({ params, redirect, error, $axios, $claims }) {
+    const data = await fetch("https://bibliotech.bymotto.com/api/hubs")
+      .then((res) => res.json())
+      .then((data) => data.data);
+
+    const homeConfig = await fetch("http://bibliotech.bymotto.com/api/config")
+      .then((res) => res.json())
+      .then((data) => data.data);
+    const about = homeConfig.hide_about_us;
+    const contact = homeConfig.hide_contact;
+    const lang = homeConfig.languages_availables;
+    const title = homeConfig.title;
+    return { data, homeConfig, about, contact, lang, title };
+  },
+
+  methods: {
+    async changeLang(item) {
+      let response = await fetch(
+        `http://bibliotech.bymotto.com/api/config/${item}`
+      )
+        .then((res) => res.json())
+        .then((data) => data.data);
+      this.title = response.title;
+    },
+  },
+};
+</script>
+
 <template>
-  <Tutorial/>
+  <div>
+    <nav>
+      <ul>
+        <li v-if="about"><RouterLink :to="'/about'">About us</RouterLink></li>
+        <li v-if="contact">
+          <RouterLink :to="'/contact'">Contact</RouterLink>
+        </li>
+        <li><RouterLink :to="'/filter'">Filtro</RouterLink></li>
+      </ul>
+      <ul>
+        <li v-for="(item, i) in lang" :key="i" @click="changeLang(item)">
+          {{ item }}
+        </li>
+      </ul>
+    </nav>
+    <br /><br />
+    <h2>{{ title }}</h2>
+    <br /><br />
+    <div class="productContainer">
+      <div class="product" v-for="(product, i) in data" :key="i">
+        <RouterLink :to="`/details/${product.id}`">
+          <div>
+            {{ product.name }}
+          </div>
+        </RouterLink>
+        <div v-html="product.description"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'IndexPage'
+<style scoped>
+nav {
+  display: flex;
+  flex-direction: row;
 }
-</script>
+
+ul {
+  display: flex;
+  flex-direction: row;
+  margin-left: 1rem;
+}
+
+li {
+  margin-left: 1rem;
+  list-style: none;
+  cursor: pointer;
+}
+</style>
